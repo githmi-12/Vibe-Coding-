5ccfb0e1e6b99b3931032bc0b2d3fa7799b5b382
githmi-12 <githmime12@gmail.com>
2026-03-06 20:40:42 +0530

    Grade Calculator - Task 03

--- grade_calculator.py
+++ grade_calculator.py
@@ -1,6 +1,8 @@
 def get_valid_name():
     while True:
-        name = input("Enter Student name: ")
+        name = input("Enter Student name (or 'exit' to stop): ")
+        if name.lower() == "exit":
+            return "exit"
         if name and len(name) > 2 and name.isalpha():
             return name
         print("The student’s name you entered is invalid. Please re-enter")
@@ -20,25 +22,28 @@ def get_valid_mark(subject_num):
             print("The subject mark you entered is invalid. Please re-enter")
 
 def main():
-    name = get_valid_name()
-    
-    marks = []
-    for i in range(1, 4):
-        marks.append(get_valid_mark(i))
-    
-    average = sum(marks) / len(marks)
-    rounded_average = round(average, 4)
-    
-    if rounded_average >= 75:
-        grade = "A"
-    elif rounded_average >= 60:
-        grade = "B"
-    elif rounded_average > 40:
-        grade = "C"
-    else:
-        grade = "F"
+    while True:
+        name = get_valid_name()
+        if name.lower() == "exit":
+            break
+            
+        marks = []
+        for i in range(1, 4):
+            marks.append(get_valid_mark(i))
         
-    print(f"Grade: {grade}")
+        average = sum(marks) / len(marks)
+        rounded_average = round(average, 4)
+        
+        if rounded_average >= 75:
+            grade = "A"
+        elif rounded_average >= 60:
+            grade = "B"
+        elif rounded_average > 40:
+            grade = "C"
+        else:
+            grade = "F"
+            
+        print(f"Grade: {grade}")
 
 if __name__ == "__main__":
     main()
